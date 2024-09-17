93d1520c4115030b692034fabe7b4ddff6028622
Ranidu Nadeesha <ranidunadeesha@gmail.com>
2024-09-18 00:00:24 +0800

    electron renderer added

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log('Hello from Eleuthia 👋')
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+  const window = new BrowserWindow({
+    width: 800,
+    height: 600,
+    darkTheme: true
+  });
+
+  window.loadFile("index.html");
+};
+
+app.whenReady().then(() => {
+  createWindow();
+});
